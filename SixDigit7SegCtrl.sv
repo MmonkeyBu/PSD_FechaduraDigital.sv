a8f97203a14e51419f10e3a45d9fef4dfe2fd719
Rodrigues Lima <135235806+Rodriguesmath@users.noreply.github.com>
2025-08-06 15:02:20 -0300

    Update SixDigit7SegCtrl.sv

--- SixDigit7SegCtrl.sv
+++ SixDigit7SegCtrl.sv
@@ -5,10 +5,6 @@ input logic enable,
 input bcdPac_t bcd_packet,
 output logic [6:0] HEX0, HEX1,HEX2, HEX3, HEX4, HEX5
 );
-
-    logic tecla_valid_d1; 
-    logic tecla_valid_posedge;
-
     // Função para converter BCD para 7 segmentos
     function logic [6:0] bcd_to_7seg(input logic [4:0] bcd);
         case (bcd)
@@ -22,24 +18,25 @@ output logic [6:0] HEX0, HEX1,HEX2, HEX3, HEX4, HEX5
             4'h7: bcd_to_7seg = 7'b0000111;
             4'h8: bcd_to_7seg = 7'b1111111;
             4'h9: bcd_to_7seg = 7'b1100111;
-            4'hA: bcd_to_7seg = 7'b1110111; 
-            4'hB: bcd_to_7seg = 7'b1111100; 
-            4'hC: bcd_to_7seg = 7'b0111001; 
-            4'hD: bcd_to_7seg = 7'b1011110;  
-            4'hF: bcd_to_7seg = 7'b1000000; 
+            4'hA: bcd_to_7seg = 7'b1000000; 
+            4'hB: bcd_to_7seg = 7'b0000000; 
+           
 
-            default: bcd_to_7seg = 7'b1111111; 
+            default: bcd_to_7seg = ~(7'b1111111); 
         endcase
     endfunction
 
     always_ff @(posedge clk or posedge rst) begin
         if (rst) begin
-            tecla_valid_d1 <= 1'b0; 
+					 HEX0 = ~bcd_to_7seg(4'd16);
+                HEX1 = ~bcd_to_7seg(4'd16);
+                HEX2 = ~bcd_to_7seg(4'd16);
+                HEX3 = ~bcd_to_7seg(4'd16);
+                HEX4 = ~bcd_to_7seg(4'd16);
+                HEX5 = ~bcd_to_7seg(4'd16);
         end else begin
 
-            tecla_valid_d1 <= enable;
-
-            if (tecla_valid_posedge) begin 
+            if (enable) begin 
                 
                 HEX0 = ~bcd_to_7seg(bcd_packet.BCD0);
                 HEX1 = ~bcd_to_7seg(bcd_packet.BCD1);
@@ -52,6 +49,5 @@ output logic [6:0] HEX0, HEX1,HEX2, HEX3, HEX4, HEX5
         end
     end
 
-    assign tecla_valid_posedge = enable && !tecla_valid_d1;
 
 endmodule
