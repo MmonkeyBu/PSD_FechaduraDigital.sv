bc98c8a6e161bee02904d1f2c2c43d2403cbf066
Rodrigues Lima <135235806+Rodriguesmath@users.noreply.github.com>
2025-08-08 11:56:10 -0300

    Update update_master.sv

--- update_master.sv
+++ update_master.sv
@@ -14,42 +14,4 @@ module update_master (
     output pinPac_t    new_master_pin  
 );
 
-    always_ff @(posedge clk or posedge rst) begin
-        if (rst) begin
-            new_master_pin.status <= 1'b0;
-            new_master_pin.digit1 <= 4'hF;  
-            new_master_pin.digit2 <= 4'hF;  
-            new_master_pin.digit3 <= 4'hF;  
-            new_master_pin.digit4 <= 4'hF;  
-        end else begin
-            if (enable) begin 
-                
-                if (pin_in.status) begin
-                    
-                    if ((pin_in.digit1 <= 4'd9) && (pin_in.digit2 <= 4'd9) && 
-                        (pin_in.digit3 <= 4'd9) && (pin_in.digit4 <= 4'd9)) begin
-                        
-                        new_master_pin.status <= 1'b1;
-                        new_master_pin.digit1 <= pin_in.digit1;
-                        new_master_pin.digit2 <= pin_in.digit2;
-                        new_master_pin.digit3 <= pin_in.digit3;
-                        new_master_pin.digit4 <= pin_in.digit4;
-                    end else begin
-                        // PIN inválido: mantém status em 0
-                        new_master_pin.status <= 1'b0;
-                    end
-                end else begin                    
-                    new_master_pin.status <= 1'b0;
-                end
-              
-            end else begin
-                new_master_pin.status <= 1'b0;
-                new_master_pin.digit1 <= 4'hF;  
-                new_master_pin.digit2 <= 4'hF;  
-                new_master_pin.digit3 <= 4'hF;  
-                new_master_pin.digit4 <= 4'hF;  
-            end
-        end
-    end
-
-endmodule
+  
